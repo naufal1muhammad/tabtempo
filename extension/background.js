1c49462d0102a25aff923bfbe07aac1006caeb24
maxnaufal99 <naufal@innocel.com.my>
2025-07-04 15:54:14 +0800

    added alarms in background.js

--- extension/background.js
+++ extension/background.js
@@ -1,1 +1,24 @@
-<script>Empty file for now</script>
+// Create a repeating alarm named "pollActiveTab" every 1 minute
+chrome.alarms.create("pollActiveTab", {
+  periodInMinutes: 1
+});
+
+// Listen for the alarm
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "pollActiveTab") {
+    // Query the active tab in the current window
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length === 0) return;
+      const activeTab = tabs[0];
+
+      // For now, just log the URL and title
+      console.log("Polled active tab:", {
+        id: activeTab.id,
+        title: activeTab.title,
+        url: activeTab.url
+      });
+
+      // TODO: send this data to storage or over SignalR
+    });
+  }
+});

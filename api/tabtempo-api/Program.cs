d37208cd698fbb081b0610947b034d3db64ca39a
maxnaufal99 <naufal@innocel.com.my>
2025-07-23 09:18:48 +0800

    Added CORS feature to communicate with React viewer

--- api/tabtempo-api/Program.cs
+++ api/tabtempo-api/Program.cs
@@ -1,15 +1,28 @@
 using tabtempo_api.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TabTempoDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ViewerPolicy", policy =>
+    {
+        policy
+        .WithOrigins("http://localhost:5173") // <— your Vite dev server origin
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
 app.UseRouting();
+app.UseCors("ViewerPolicy");
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
